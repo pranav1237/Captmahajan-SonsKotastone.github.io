8514240ffe7af4603d0d842b7a5c42a4bddee0c4
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-06-13 20:25:59 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+{
+  "name": "kotastone-site",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.21.2"
+  }
+}
